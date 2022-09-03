a552f07aa2fc320ed8f995fa40dd033f72053d64
Aurora Pariseau <aurorapariseau@gmail.com>
2022-09-02 22:24:07 -0500

    Adding configuration information and assert only when not using built-in training data

--- driver.py
+++ driver.py
@@ -8,10 +8,19 @@ from numba import njit
 import config
 
 
+def debug(msg):
+    if True:
+        print(msg)
+
+
 def set_configuration_options():
     custom_config = config.settings
     for k, v in custom_config.items():
-        assert v
+        print(f"Unit testing config\n\tkey: {k}")
+        if k.startswith('training'):
+            assert (v or mnist_data)
+        else:
+            assert v
     return custom_config
 
 
@@ -26,6 +35,9 @@ def create_hidden_layer(layer_size: int):
     x = np.random.randint(low=-10, high=10, size=(layer_size, 3))
     y = np.random.rand(layer_size, 3)
     hidden_layer = np.add(x, y)
+    print("Adding new layer:\n\t")
+    print(hidden_layer)
+    exit()
     return hidden_layer
 
 
@@ -44,12 +56,16 @@ def find_loss(batch_images, batch_labels):
 
 if __name__ == '__main__':
     CONFIG = set_configuration_options()
-    assert os.path.exists(CONFIG['training_data_path'])
 
     # for custom data set
-    training_data_path = CONFIG['training_data_path']
-    training_label_file = CONFIG['training_data_label_file']
-    assert os.path.exists(training_label_file)
+    training_data_path = CONFIG['training_data_path'] or mnist_data
+    training_label_file = CONFIG['training_data_label_file'] or mnist_data
+    # ignore if using MNIST training set
+    if training_data_path != mnist_data:
+        assert os.path.exists(training_label_file)
+
+    if training_label_file != mnist_data:
+        assert os.path.exists(training_label_file)
 
     net = [create_activation_layer(CONFIG['layer_sizes'][0])]
     for layer_size in range(1, len(CONFIG['layer_sizes']) - 1):
