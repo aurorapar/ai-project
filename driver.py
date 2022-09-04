Fixed forward propagation algorithm (summed entire array into single value). Added ReL function to hidden layers. Still need to do softmax on final.

driver.py
@@ -42,8 +42,10 @@ def forward_propagate(a, w, b):
     # debug(f"Propagating to layer {index+1}")
     # in order to fit into dimension of next layer, use next layer input as first parameter
     # z = np.sum(previous_layer[1].reshape(-1, 1) @ previous_layer[0].reshape(1, -1), axis=1)
-    z = np.sum(w @ a)
+    z = np.sum(w @ a, axis=1)
     next_layer = np.add(z, b)
+    # ReL function
+    next_layer = np.array([max(0, x) for x in next_layer])
     return next_layer
 
 
@@ -65,6 +67,7 @@ def process_batches(network):
             # network[l][n=0, w=1, b=2]
             network[0][0] = image / 255
             for layer_index in range(1, len(network)):
+
                 inputs = network[layer_index-1][0].reshape(1, -1)
                 weights = network[layer_index-1][1].reshape(-1, 1)
                 biases = network[layer_index-1][2]
@@ -73,7 +76,7 @@ def process_batches(network):
                     network[layer_index][0] = new_output
                 else:
                     network[layer_index] = new_output
-            debug(network[-1])
+            print(network[-1])
             exit()
 
 